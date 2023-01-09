87df8607177d50c6cf22d296e4e55c0c0861016f
Laxmi Ramesh Mallur <112336489+LaxmiMallur070@users.noreply.github.com>
2023-01-09 11:33:41 +0530

    Update SpeedConverter.java

--- SpeedConverter.java
+++ SpeedConverter.java
@@ -6,7 +6,7 @@ Example: toMilesPerHour(10.25); → should return value 6
 		 
 2nd method: Write another method called printConversion with 1 parameter of type double with the name kilometersPerHour. This method should not return anything (void) and it needs to calculate milesPerHour from the kilometersPerHour parameter. Then it needs to print a message in the format "XX km/h = YY mi/h". XX represents the original value kilometersPerHour. YY represents the rounded milesPerHour from the kilometersPerHour parameter. If the parameter kilometersPerHour is < 0 then print the text "Invalid Value".
 Example: printConversion(10.25); → should print the following text (into the console - System.out): 10.25 km/h = 6 mi/h
-		 printConversion(-5.6); → should print the following text (into the console - System.out): Invalid Value
+	 printConversion(-5.6); → should print the following text (into the console - System.out): Invalid Value
 **************/
 
 
